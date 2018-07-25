21468d0b611afc2fd4d1d90b45c18815a81cdcb2
Carmen Ye <carmen_ye@amp.com.au>
2018-07-25 16:36:23 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Http } from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'weather app';
+  private apiKey = '';
+  private apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=' + this.apiKey;
+
+  constructor (private http: Http) {
+  }
+
+  ngOnInit() {
+    this.http.get(this.apiUrl).subscribe( data => {
+      console.log('success');
+      console.log(data);
+    }, error => {
+      console.log('error');
+    });
+  }
+
 }
